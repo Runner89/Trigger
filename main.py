66446009f8f9ccdee811a0bd94841641c7f64c62
Runner89 <alain.login@hotmail.com>
2026-01-09 10:53:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1579,6 +1579,7 @@ def webhook():
                             return jsonify({
                                 "error": True,
                                 "msg": "BO-Grösse konnte nicht gelesen werden oder ist 0. Keine Order gesetzt.",
+                                "wert_fb": wert_fb,
                                 "bot_nr": bot_nr,
                                 "position_side": position_side,
                                 "logs": logs
